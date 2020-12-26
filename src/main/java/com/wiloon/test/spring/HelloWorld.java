a0020414d4c19cfc678a2dc7ec98ec03b8345415
wiloon <wiloon.wy@gmail.com>
2020-12-26 18:16:46 +0800

    spring session redis

--- src/main/java/com/wiloon/test/spring/HelloWorld.java
+++ src/main/java/com/wiloon/test/spring/HelloWorld.java
@@ -4,14 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class HelloWorld {
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     @ResponseBody
-    public Map<String, Object> hello() {
+    public Map<String, Object> hello(HttpSession session) {
+        System.out.println("session: " + session.getId());
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        } else {
+            System.out.println("uid:" + uid.toString());
+        }
+        session.setAttribute("uid", uid);
+
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("hello", "world");
         return map;
---
+++ src/main/java/com/wiloon/test/spring/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.wiloon.test.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by jack on 2017/4/27.
+ */
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("root")
+                .password(new BCryptPasswordEncoder().encode("root"))
+                .roles("ADMIN");
+    }
+
+
+}
